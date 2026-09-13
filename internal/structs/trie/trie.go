0167ab5e74da863932dced36f2e28430e35bbe7f
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2026-09-13 12:42:36 +0200

    feat: added Trie.FindMatches()

--- internal/structs/trie/trie.go
+++ internal/structs/trie/trie.go
@@ -3,13 +3,10 @@ package trie
 import (
 	"fmt"
 	"regexp"
+	"sort"
 )
 
 const pattern string = "^[a-zA-Z0-9_.-]{1,32}$"
-
-type Trie struct {
-	RootNode *Node
-}
 
 type Node struct {
 	Char     rune
@@ -22,6 +19,10 @@ func NewNode(r rune) *Node {
 		Char:     r,
 		Children: make(map[rune]*Node),
 	}
+}
+
+type Trie struct {
+	RootNode *Node
 }
 
 func NewTrie() *Trie {
@@ -66,12 +67,12 @@ func (t *Trie) SearchWord(word string) (bool, error) {
 		}
 
 		next, exists := current.Children[r]
-        if !exists {
-            return false, nil
-        }
-        current = next
-    }
-    return current.IsEnd, nil
+		if !exists {
+			return false, nil
+		}
+		current = next
+	}
+	return current.IsEnd, nil
 }
 
 func (t *Trie) SearchPrefix(word string) (bool, error) {
@@ -86,10 +87,64 @@ func (t *Trie) SearchPrefix(word string) (bool, error) {
 		}
 
 		next, exists := current.Children[r]
-        if !exists {
-            return false, nil
-        }
-        current = next
-    }
-    return true, nil
+		if !exists {
+			return false, nil
+		}
+		current = next
+	}
+	return true, nil
+}
+
+type queueItem struct {
+	node   *Node
+	prefix string
+}
+
+func (t *Trie) FindMatches(prefix string) ([]string, error) {
+	if len(prefix) < 1 || len(prefix) > 32 {
+		return []string{}, fmt.Errorf("word length must be between 1 and 32 characters")
+	}
+
+	for _, r := range prefix {
+		if !isValidRune(r) {
+			return nil, fmt.Errorf("invalid character: %q", r)
+		}
+	}
+
+	current := t.RootNode
+	for _, r := range prefix {
+		next, exists := current.Children[r]
+		if !exists {
+			return nil, nil
+		}
+		current = next
+	}
+
+	var results []string
+	queue := []queueItem{{node: current, prefix: prefix}}
+
+	for len(queue) > 0 {
+		currItem := queue[0]
+		queue = queue [1:]
+
+		if currItem.node.IsEnd {
+			results = append(results, currItem.prefix)
+		}
+
+		var keys []rune
+		for k := range currItem.node.Children {
+			keys = append(keys, k)
+		}
+		sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
+
+		for _, r := range keys {
+			childNode := currItem.node.Children[r]
+			queue = append(queue, queueItem{
+				node:	childNode,
+				prefix: currItem.prefix + string(r),
+			})
+		}
+	}
+
+	return results, nil
 }
