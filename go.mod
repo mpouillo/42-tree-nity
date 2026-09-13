faa2d808c8fcf2a3cc40b0734361637ae8b2d3bf
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2026-09-13 17:35:04 +0200

    test: updated tests to match updated trie.go functions

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/mpouillo/42-tree-nity
 
 go 1.27.1
+
+require github.com/stretchr/testify v1.12.1 // direct
+
+require go.yaml.in/yaml/v3 v3.0.5 // indirect
--- internal/structs/trie/trie_test.go
+++ internal/structs/trie/trie_test.go
@@ -1,8 +1,8 @@
 package trie
 
 import (
-	"slices"
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewTrie(t *testing.T) {
@@ -14,210 +14,59 @@ func TestNewTrie(t *testing.T) {
 	if trie.RootNode == nil {
 		t.Fatal("Expected RootNode to be initialized, got nil")
 	}
-	if trie.RootNode.Char != -1 {
-		t.Errorf("Expected root node character to be '-1', got %q", trie.RootNode.Char)
+	if trie.RootNode.Value != nil {
+		t.Errorf("Expected root node character to be nil, got %q", trie.RootNode.Value)
 	}
+}
+
+type client struct {
+	id     string
+	offset int64
 }
 
 func TestInsert(t *testing.T) {
-	tests := []struct {
-		name     string
-		input    string
-		expected []string
-	}{
-		{
-			name:     "Insert single character string",
-			input:    "-",
-			expected: []string{"-"},
-		},
-		{
-			name:     "Insert single lowercase word",
-			input:    "cat",
-			expected: []string{"c", "a", "t"},
-		},
-		{
-			name:     "Insert uppercase word with spaces",
-			input:    "L33t",
-			expected: []string{"L", "3", "3", "t"},
-		},
-	}
+	t.Run("Basic insert", func(t *testing.T) {
+		key := "user"
+		value := client{"client0", 0}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			trie := NewTrie()
-			err := trie.Insert(tt.input)
+		trie := NewTrie()
+		trie.Insert(key, value)
 
-			if err != nil {
-				t.Fatalf("Unexpected error during Insert(%q): %v", tt.input, err)
-			}
+		current := trie.RootNode
+		for _, char := range key {
+			current = current.Children[char]
+		}
+		assert.Equal(t, current.Value, value)
+	})
 
-			current := trie.RootNode
-			for _, char := range tt.expected {
-				if current.Children[rune(char[0])] == nil {
-					t.Fatalf("Expected child node for character %q, but found nil", char)
-				}
-				current = current.Children[rune(char[0])]
-				if current.Char != rune(char[0]) {
-					t.Errorf("Expected node character %q, got %q", char, current.Char)
-				}
-			}
-		})
-	}
+	t.Run("Root insert", func(t *testing.T) {
+		key := ""
+		value := client{"client0", 0}
+
+		trie := NewTrie()
+		trie.Insert(key, value)
+
+		current := trie.RootNode
+		for _, char := range key {
+			current = current.Children[char]
+		}
+		assert.Equal(t, current.Value, value)
+	})
 }
 
-func TestSearchWord(t *testing.T) {
-	tests := []struct {
-		name     string
-		input    string
-		expected bool
-	}{
-		{
-			name:     "Search full match",
-			input:    "doggo",
-			expected: true,
-		},
-		{
-			name:     "Search partial match",
-			input:    "dog",
-			expected: false,
-		},
-		{
-			name:     "Search invalid match",
-			input:    "cat",
-			expected: false,
-		},
+func TestSearch(t *testing.T) {
+	trie := NewTrie()
+	trie.Insert("user", client{"client0", 0})
+	trie.Insert("user.action", client{"client1", 3})
+	trie.Insert("", client{"client2", 99})
+
+	got, err := trie.Search("user.action")
+	want := []any{
+		client{"client1", 3},
+		client{"client2", 99},
+		client{"client0", 0},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			trie := NewTrie()
-			err := trie.Insert("doggo")
-
-			if err != nil {
-				t.Fatalf("Unexpected error during Insert(\"doggo\"): %v", err)
-			}
-
-			got, err := trie.SearchWord(tt.input)
-			want := tt.expected
-
-			if err != nil {
-				t.Fatalf("Unexpected error during Search(%q): %v", tt.input, err)
-			}
-
-			if got != want {
-				t.Errorf("got %t want %t", got, want)
-			}
-
-		})
-	}
-}
-
-func TestSearchPrefix(t *testing.T) {
-	tests := []struct {
-		name     string
-		input    string
-		expected bool
-	}{
-		{
-			name:     "Search full match",
-			input:    "boogie-woogie.",
-			expected: true,
-		},
-		{
-			name:     "Search partial match",
-			input:    "boogie",
-			expected: true,
-		},
-		{
-			name:     "Search invalid match",
-			input:    "woogie",
-			expected: false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			trie := NewTrie()
-			err := trie.Insert("boogie-woogie.")
-
-			if err != nil {
-				t.Fatalf("Unexpected error during Insert(\"doggo\"): %v", err)
-			}
-
-			got, err := trie.SearchPrefix(tt.input)
-			want := tt.expected
-
-			if err != nil {
-				t.Fatalf("Unexpected error during Search(%q): %v", tt.input, err)
-			}
-
-			if got != want {
-				t.Errorf("got %t want %t", got, want)
-			}
-
-		})
-	}
-}
-
-func TestFindMatches(t *testing.T) {
-	tests := []struct {
-		name     string
-		input    string
-		expected []string
-	}{
-		{
-			name:     "Search full match",
-			input:    "boogie-woogie",
-			expected: []string{"boogie-woogie"},
-		},
-		{
-			name:     "Search partial match",
-			input:    "boogie",
-			expected: []string{"boogie-woogie", "boogiepop"},
-		},
-		{
-			name:     "Search invalid match",
-			input:    "rocketship",
-			expected: nil,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			trie := NewTrie()
-			for _, word := range []string{"boogie-woogie", "boogiepop", "boolean", "woogie"} {
-				err := trie.Insert(word)
-
-				if err != nil {
-					t.Fatalf("Unexpected error during Insert(\"%v\"): %v", word, err)
-				}
-			}
-
-			got, err := trie.FindMatches(tt.input)
-			want := tt.expected
-
-			if err != nil {
-				t.Fatalf("Unexpected error during Search(%q): %v", tt.input, err)
-			}
-
-			if !hasSameElements(got, want) {
-				t.Errorf("got %v want %v", got, want)
-			}
-
-		})
-	}
-}
-
-func hasSameElements(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-
-	aCopy := slices.Clone(a)
-	bCopy := slices.Clone(b)
-
-	slices.Sort(aCopy)
-	slices.Sort(bCopy)
-
-	return slices.Equal(aCopy, bCopy)
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, want, got)
 }
