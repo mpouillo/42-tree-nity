3fdfc9b3969cb805bb9f2273c0cc60bf5166bcf1
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2026-09-26 13:55:14 +0200

    feat(consumer): added NewConsumer() function

--- internal/consumer/consumer.go
+++ internal/consumer/consumer.go
@@ -6,11 +6,27 @@ import (
 )
 
 type Consumer struct {
-	ID      string
-	Topic   string
-	Offset  uint32
-	Prefix  string
-	ipcPath fifo.Fifo
+	ID     string
+	Topic  string
+	Offset uint32
+	Prefix string
+	Fifo   *fifo.Fifo
+}
+
+func NewConsumer(id, ipcPath, topic, prefix string, offset uint32) (*Consumer, error) {
+	fifo, err := fifo.Open(ipcPath, fifo.Write)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Consumer{
+		ID: id,
+		Topic: topic,
+		Offset: offset,
+		Prefix: prefix,
+		Fifo: fifo,
+	}, nil
 }
 
 func (c *Consumer) Deliver(msg message.Message) {}
